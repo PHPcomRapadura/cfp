c0ca896d03865dc40d32b38ce300cdaad122c8a1
AlissonSilva12 <alisson.sousa12@gmail.com>
2017-05-31 21:06:30 -0300

    alteracao no layout views

--- resources/views/talk/listall.blade.php
+++ resources/views/talk/listall.blade.php
@@ -48,7 +48,7 @@
 	@if(isset($talks))	
 	<div class="row">
 		<div class="col-md-10 col-md-offset-1">
-			<div class="panel panel-primary">
+			<div class="panel panel-danger">
 				<div class="panel-heading" id="cor-padrao">
 					<strong><i class="fa fa-list-alt" aria-hidden="true"></i> Listagem de talks enviadas</strong> 
 				</div>
