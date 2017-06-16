038045e02b52111475f98209abb6a143278d67f9
Dorian Neto <doriansampaioneto@gmail.com>
2017-06-15 22:31:36 -0300

    improvement: layout and icons changed from dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,32 +5,26 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="panel panel-danger">
-                    <div class="panel-heading" id="cor-padrao"><strong>Seja bem-vindo Sr.(a) {{ Auth::user()->apelido }}</strong></div>
+                    <div class="panel-heading" id="cor-padrao">
+                        <strong>Seja bem-vindo Sr.(a) {{ Auth::user()->apelido }}</strong>
+                    </div>
                     <div class="panel-body">
-
-                    @if(empty(Auth::user()->aeroporto)|| empty(Auth::user()->sexo_id)|| empty(Auth::user()->alimentacao))
-                         <div class="col-md-12">
-                            <div class="alert alert-danger col-md-8 col-md-offset-2">
-                            <ul>
-                                <li><i class="fa fa-hand-paper-o" aria-hidden="true"></i> Há dados no seu perfil que precisam ser preenchidos!</li>
-                            </ul>
-                        </div>
-                         </div>
-                    @endif
+                        @if(empty(Auth::user()->aeroporto) || empty(Auth::user()->sexo_id) || empty(Auth::user()->alimentacao))
+                            <div class="col-md-12">
+                                <div class="alert alert-warning col-md-8 col-md-offset-2">
+                                    <i class="fa fa-exclamation-triangle" aria-hidden="true"></i> Há dados no seu perfil que precisam ser preenchidos! Por favor, <a href="{{ route('user.edit', Auth::user()->id) }}"><u>complete seu cadastro</u></a>.
+                                </div>
+                            </div>
+                        @endif
 
                         @can('talks-view')
-                            <div class="col-md-6">
+                            <div class="col-md-4">
                                 <div class="thumbnail text-center">
-                                    <img src="{{URL::asset('/img/user-talk.png')}}" alt="Talk">
+                                    <i class="fa fa-microphone fa-5x"></i>
                                     <div class="caption">
-                                        <h4>
-                                            <strong>
-                                                <i class="fa fa-microphone" aria-hidden="true"></i>
-                                                Submeta a sua palestra
-                                            </strong>
-                                        </h4>
+                                        <h4><strong>Submeta sua palestra</strong></h4>
                                         <p>
-                                            <a href="{{ url('/talk') }}" class="btn btn-primary btn-sm" role="button">
+                                            <a href="{{ url('/talk') }}" class="btn btn-primary" role="button">
                                                 <i class="fa fa-sign-in" aria-hidden="true"></i> Acessar
                                             </a>
                                         </p>
@@ -42,16 +36,11 @@
                         @can('talks-all')
                             <div class="col-md-4">
                                 <div class="thumbnail text-center">
-                                    <img src="{{URL::asset('/img/talks-various.jpg')}}" alt="Talk">
+                                    <i class="fa fa-comments fa-5x"></i>
                                     <div class="caption">
-                                        <h4>
-                                            <strong>
-                                                <i class="fa fa-microphone" aria-hidden="true"></i>
-                                                Visualizar Paslestras Submetidas
-                                            </strong>
-                                        </h4>
+                                        <h4><strong>Paslestras submetidas</strong></h4>
                                         <p>
-                                            <a href="{{ url('/talks') }}" class="btn btn-primary btn-sm" role="button">
+                                            <a href="{{ url('/talks') }}" class="btn btn-primary" role="button">
                                                 <i class="fa fa-sign-in" aria-hidden="true"></i> Acessar
                                             </a>
                                         </p>
@@ -60,55 +49,52 @@
                             </div>
                         @endcan
 
-                    @can('users-update')
-                        <div class="col-md-6">
-
-                            <div class="thumbnail text-center">
-
-                                <img src="{{URL::asset('/img/user-1.jpg')}}" alt="Perfil">
-
-                                <div class="caption">
-                                    <h4><strong><i class="fa fa-edit" aria-hidden="true"></i> Editar seu perfil</strong>
-                                    </h4>
-                                    <p><a href="{{ route('user.edit', Auth::user()->id) }}"
-                                          class="btn btn-primary btn-sm" role="button"><i class="fa fa-sign-in" aria-hidden="true"></i> Acessar</a></p>
+                        @can('users-update')
+                            <div class="col-md-4">
+                                <div class="thumbnail text-center">
+                                    <i class="fa fa-user fa-5x"></i>
+                                    <div class="caption">
+                                        <h4><strong>Edite seu perfil</strong></h4>
+                                        <p>
+                                            <a href="{{ route('user.edit', Auth::user()->id) }}" class="btn btn-primary" role="button">
+                                                <i class="fa fa-sign-in" aria-hidden="true"></i> Editar
+                                            </a>
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
                         @endcan
 
                         @can('users-view')
-                        <div class="col-md-4">
-
-                            <div class="thumbnail text-center">
-
-                                <img src="{{URL::asset('/img/users-2-icon.png')}}" alt="Perfil">
-
-                                <div class="caption">
-                                    <h4><strong><i class="fa fa-edit" aria-hidden="true"></i> Visualizar perfis dos palestrantes</strong>
-                                    </h4>
-                                    <p><a href="{{ route('user.index') }}"
-                                          class="btn btn-primary btn-sm" role="button"><i class="fa fa-sign-in" aria-hidden="true"></i> Acessar</a></p>
+                            <div class="col-md-4">
+                                <div class="thumbnail text-center">
+                                    <i class="fa fa-users fa-5x"></i>
+                                    <div class="caption">
+                                        <h4><strong>Visualizar perfis dos palestrantes</strong></h4>
+                                        <p>
+                                            <a href="{{ route('user.index') }}" class="btn btn-primary" role="button">
+                                                <i class="fa fa-sign-in" aria-hidden="true"></i> Acessar
+                                            </a>
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
                         @endcan
 
                         @can('events-view')
-                        <div class="col-md-4">
-
-                            <div class="thumbnail text-center">
-
-                                <img src="{{URL::asset('/img/events-1.png')}}" alt="Eventos">
-
-                                <div class="caption">
-                                    <h4><strong><i class="fa fa-users" aria-hidden="true"></i> Controle de
-                                            Eventos</strong></h4>
-                                    <p><a href="{{ url('/event') }}" class="btn btn-primary btn-sm" role="button"><i
-                                                    class="fa fa-sign-in" aria-hidden="true"></i> Acessar</a></p>
+                            <div class="col-md-4">
+                                <div class="thumbnail text-center">
+                                    <i class="fa fa-calendar fa-5x"></i>
+                                    <div class="caption">
+                                        <h4><strong>Controle de Eventos</strong></h4>
+                                        <p>
+                                            <a href="{{ url('/event') }}" class="btn btn-primary" role="button">
+                                                <i class="fa fa-sign-in" aria-hidden="true"></i> Acessar
+                                            </a>
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
                         @endcan
 
                     </div>
