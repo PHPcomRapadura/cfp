d315d575113d799c21881c9584eb0929caf951b0
Dorian Neto <doriansampaioneto@gmail.com>
2017-06-16 11:36:57 -0300

    fix: arrangement of blocks from dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,88 +17,91 @@
                             </div>
                         @endif
 
-                        @can('talks-create')
-                            <div class="col-md-4">
-                                <div class="thumbnail text-center">
-                                    <i class="fa fa-microphone fa-5x"></i>
-                                    <div class="caption">
-                                        <h4><strong>Submeta sua palestra</strong></h4>
-                                        <p>
-                                            <a href="{{ route('talk.create') }}" class="btn btn-primary" role="button">
-                                                <i class="fa fa-sign-in" aria-hidden="true"></i> Acessar
-                                            </a>
-                                        </p>
+                        <div class="row">
+                            @can('talks-all')
+                                <div class="col-md-4">
+                                    <div class="thumbnail text-center">
+                                        <i class="fa fa-comments fa-5x"></i>
+                                        <div class="caption">
+                                            <h4><strong>Palestras submetidas</strong></h4>
+                                            <p>
+                                                <a href="{{ route('talks.all') }}" class="btn btn-primary" role="button">
+                                                    <i class="fa fa-sign-in" aria-hidden="true"></i> Acessar
+                                                </a>
+                                            </p>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        @endcan
+                            @endcan
 
-                        @can('talks-all')
-                            <div class="col-md-4">
-                                <div class="thumbnail text-center">
-                                    <i class="fa fa-comments fa-5x"></i>
-                                    <div class="caption">
-                                        <h4><strong>Palestras submetidas</strong></h4>
-                                        <p>
-                                            <a href="{{ route('talks.all') }}" class="btn btn-primary" role="button">
-                                                <i class="fa fa-sign-in" aria-hidden="true"></i> Acessar
-                                            </a>
-                                        </p>
+                            @can('events-view')
+                                <div class="col-md-4">
+                                    <div class="thumbnail text-center">
+                                        <i class="fa fa-calendar fa-5x"></i>
+                                        <div class="caption">
+                                            <h4><strong>Controle de Eventos</strong></h4>
+                                            <p>
+                                                <a href="{{ route('event.index') }}" class="btn btn-primary" role="button">
+                                                    <i class="fa fa-sign-in" aria-hidden="true"></i> Acessar
+                                                </a>
+                                            </p>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        @endcan
+                            @endcan
 
-                        @can('users-update')
-                            <div class="col-md-4">
-                                <div class="thumbnail text-center">
-                                    @if (Storage::exists('public/uploads/' . Auth::user()->foto))
-                                        <img class="img-responsive img-circle" width="70" src="{{ asset('storage/uploads/' . Auth::user()->foto) }}">
-                                    @endif
-                                    <div class="caption">
-                                        <h4><strong>Edite seu perfil</strong></h4>
-                                        <p>
-                                            <a href="{{ route('user.edit', Auth::user()->id) }}" class="btn btn-primary" role="button">
-                                                <i class="fa fa-sign-in" aria-hidden="true"></i> Editar
-                                            </a>
-                                        </p>
+                            @can('users-view')
+                                <div class="col-md-4">
+                                    <div class="thumbnail text-center">
+                                        <i class="fa fa-users fa-5x"></i>
+                                        <div class="caption">
+                                            <h4><strong>Visualizar palestrantes</strong></h4>
+                                            <p>
+                                                <a href="{{ route('user.index') }}" class="btn btn-primary" role="button">
+                                                    <i class="fa fa-sign-in" aria-hidden="true"></i> Acessar
+                                                </a>
+                                            </p>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        @endcan
+                            @endcan
+                        </div>
 
-                        @can('users-view')
-                            <div class="col-md-4">
-                                <div class="thumbnail text-center">
-                                    <i class="fa fa-users fa-5x"></i>
-                                    <div class="caption">
-                                        <h4><strong>Visualizar perfis dos palestrantes</strong></h4>
-                                        <p>
-                                            <a href="{{ route('user.index') }}" class="btn btn-primary" role="button">
-                                                <i class="fa fa-sign-in" aria-hidden="true"></i> Acessar
-                                            </a>
-                                        </p>
+                        <div class="row">
+                            @can('talks-create')
+                                <div class="col-md-4">
+                                    <div class="thumbnail text-center">
+                                        <i class="fa fa-microphone fa-5x"></i>
+                                        <div class="caption">
+                                            <h4><strong>Submeta sua palestra</strong></h4>
+                                            <p>
+                                                <a href="{{ route('talk.create') }}" class="btn btn-primary" role="button">
+                                                    <i class="fa fa-sign-in" aria-hidden="true"></i> Acessar
+                                                </a>
+                                            </p>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        @endcan
+                            @endcan
 
-                        @can('events-view')
-                            <div class="col-md-4">
-                                <div class="thumbnail text-center">
-                                    <i class="fa fa-calendar fa-5x"></i>
-                                    <div class="caption">
-                                        <h4><strong>Controle de Eventos</strong></h4>
-                                        <p>
-                                            <a href="{{ route('event.index') }}" class="btn btn-primary" role="button">
-                                                <i class="fa fa-sign-in" aria-hidden="true"></i> Acessar
-                                            </a>
-                                        </p>
+                            @can('users-update')
+                                <div class="col-md-4">
+                                    <div class="thumbnail text-center">
+                                        @if (Storage::exists('public/uploads/' . Auth::user()->foto))
+                                            <img class="img-responsive img-circle" width="70" src="{{ asset('storage/uploads/' . Auth::user()->foto) }}">
+                                        @endif
+                                        <div class="caption">
+                                            <h4><strong>Edite seu perfil</strong></h4>
+                                            <p>
+                                                <a href="{{ route('user.edit', Auth::user()->id) }}" class="btn btn-primary" role="button">
+                                                    <i class="fa fa-sign-in" aria-hidden="true"></i> Editar
+                                                </a>
+                                            </p>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        @endcan
-
+                            @endcan
+                        </div>
                     </div>
                 </div>
             </div>
